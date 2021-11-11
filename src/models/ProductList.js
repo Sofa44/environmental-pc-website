edc9080bba4966125a2eb5ba3d82260e18843f8f
Sofa44 <Sofa44@users.noreply.github.com>
2021-11-11 09:31:11 -0500

    Create ProductList.js

---
+++ src/models/ProductList.js
@@ -0,0 +1,31 @@
+import Keyboard from '../assets/Keyboard.png';
+import Mouse from '../assets/Mouse.png';
+import Case from '../assets/Case.png';
+import Fans from '../assets/Fans.png';
+
+export const ProductList = [
+  {
+    name: 'Keyboard',
+    image: Keyboard,
+    price: 120.0,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit",
+  },
+  {
+    name: 'Mouse',
+    image: Mouse,
+    price: 40.0,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit",
+  },
+  {
+    name: 'Case',
+    image: Case,
+    price: 256.0,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit",
+  },
+  {
+    name: 'Fans',
+    image: Fans,
+    price: 10.0,
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit",
+  }
+];
