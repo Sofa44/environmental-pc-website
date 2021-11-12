4ee964a20c4109f5a783e6e04ae03e1494ffa5c5
Sofa44 <Sofa44@users.noreply.github.com>
2021-11-12 03:46:53 +0000

    Update ProductList.js

--- src/models/ProductList.js
+++ src/models/ProductList.js
@@ -2,20 +2,20 @@ import React from 'react';
 
 export const ProductList = [
   {
-    name: 'Keyboard',
+    name: 'Wooden Keyboard',
     image:
       'https://user-images.githubusercontent.com/23085368/141319101-4d4684e4-c54d-447c-8ceb-d6326eb96926.png',
     price: 120.0,
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit',
+      "This keyboard is almost completely bio-degradable with only the wiring being non biodegredable. This is true due to the fact that main skeleton of the keyboard is wood and resin which with the correct types of both materials are environmentally friendly. This is because they are both biodegredable and don't leave hazorduos gases behind. Although the wiring might not be biodegredable, it is definetaly halogen-free which means that it won't leave behind any hazardous halogen chemicals in the environment.",
   },
   {
-    name: 'Mouse',
+    name: 'Wooden Mouse',
     image:
       'https://user-images.githubusercontent.com/23085368/141319120-5538d4ff-3bc6-4041-90ec-9b7c1c6d21ce.png',
     price: 40.0,
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similiqueaccusantium nemo autem. Veritatis obcaecati tenetur iure eius earum utmolestias architecto voluptate aliquam nihil, eveniet aliquid culpaofficia aut! Impedit sit sunt quaerat, odit, tenetur error, harumnesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo nequeerror repudiandae fuga? Ipsa laudantium molestias eos sapienteofficiis modi at sunt excepturi expedita sint? Sed quibusdamrecusandae alias error harum maxime adipisci amet laborum.Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates acumque velit',
+      "This wooden mouse is practically completely ",
   },
   {
     name: 'Case',
