511bdeb7ec6166364154bb5f42dfb73fddee2112
Sofa44 <sofa44@protonmail.com>
2021-11-11 19:12:49 -0500

    3 files modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,12 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide">
-        <img
-          src="https://user-images.githubusercontent.com/23085368/141319110-4ad86f60-4567-4c7c-aea6-ed45a6b8697c.png"
-          alt="Environment Logo"
-        />
+        <Link to="/">
+          <img
+            src="https://user-images.githubusercontent.com/23085368/141319110-4ad86f60-4567-4c7c-aea6-ed45a6b8697c.png"
+            alt="Environment Logo"
+          />{' '}
+        </Link>
         <Link to="/" id="title">
           {' '}
           Natural Technologies{' '}
